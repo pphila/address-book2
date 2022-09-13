7d0ada6da457859ee1b03d2a29e67ebb010cee9e
pphila <efl2ice89@gmail.com>
2022-09-12 20:46:42 -0700

    add fix to email address box functionality
    Co-authored-by Greg Ramsower <greg.ramsower@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ function AddressBook() {
   this.currentId = 0;
 }
 
-AddressBook.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-};
-
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
 };
 
 AddressBook.prototype.findContact = function(id) {
@@ -30,17 +30,39 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 //Business Logic for Contacts
-function Contact(firstName, lastName, phoneNumber, emailAddress, mailingAddress) {
+function Contact(firstName, lastName, phoneNumber, primaryEmail, secondaryEmail, mailingAddress) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-  this.emailAddress = emailAddress;
-  this.mailingAddress = physicalAddress;
+  this.primaryEmail = primaryEmail;
+  this.secondaryEmail = secondaryEmail;
+  this.mailingAddress = mailingAddress;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+//Contact.prototype.addEmail = function(primaryEmail, secondaryEmail) {
+  // this.primaryEmail = primaryEmail;
+  // this.secondaryEmail = secondaryEmail;
+//}
+
+// function Email(emailOne, emailTwo) {
+//  this.emailOne = emailOne;
+//  this.emailTwo = emailTwo;
+// }
+
+function displayContactDetails(event) {
+  const contact = addressBook.findContact(event.target.id);
+  document.querySelector(".first-name").innerText = contact.firstName;
+  document.querySelector(".last-name").innerText = contact.lastName;
+  document.querySelector(".phone-number").innerText = contact.phoneNumber;
+  document.querySelector(".email-address1").innerText = contact.primaryEmail;
+  document.querySelector(".email-address2").innerText = contact.secondaryEmail;
+  document.querySelector(".mailing-address").innerText = contact.mailingAddress;
+  document.querySelector("div#contact-details").removeAttribute("class");
+}
 
 // UI Logic
 let addressBook = new AddressBook();
@@ -56,7 +78,7 @@ function listContacts(addressBookToDisplay) {
     li.setAttribute("id", contact.id);
     ul.append(li);
   });
-  contactsDiv.append(ul)
+  contactsDiv.append(ul);
 }
 
 function handleFormSubmission(event) {
@@ -64,13 +86,15 @@ function handleFormSubmission(event) {
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-  const inputtedEmailAddress = document.querySelector("input#new-email-address").value;
-  const inputtedMailingAddress = document.querySelector("input#new-mailing-address").value
-  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedMailingAddress);
+  const inputtedEmailAddress1 = document.querySelector("input#new-email-address1").value;
+  const inputtedEmailAddress2 = document.querySelector("input#new-email-address2").value;
+  const inputtedMailingAddress = document.querySelector("input#new-mailing-address").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress1, inputtedEmailAddress2, inputtedMailingAddress);
   addressBook.addContact(newContact);
   listContacts(addressBook);
 }
 
 window.addEventListener("load", function () {
-  this.document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  this.document.querySelector("div#contacts").addEventListener("click", displayContactDetails);
 });
